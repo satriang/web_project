1b4d406613329bc5216e62a57b4aeeb063d1570a
SatriaNg <ngestusatria7@gmail.com>
2018-05-07 10:19:34 +0700

    web

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  var CACHE_NAME = 'static-cache';
+  var urlsToCache = [
+    '.',
+    'index.html',
+    'about.html',
+    'modernizr-custom.js',
+    'styles/main.css',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
+    'images/'
+    
+  ];
+
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        return response || fetchAndCache(event.request);
+      })
+    );
+  });
+
+  function fetchAndCache(url) {
+    return fetch(url)
+    .then(function(response) {
+      // Check if we received a valid response
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return caches.open(CACHE_NAME)
+      .then(function(cache) {
+        cache.put(url, response.clone());
+        return response;
+      });
+    })
+    .catch(function(error) {
+      console.log('Request failed:', error);
+      // You could return a custom offline 404 page here
+    });
+  }
+
+})();
